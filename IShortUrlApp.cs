bd6b0f16f9a54f3d2c8340486630e756376a4723
unknown <timurkausmanov4@gmail.com>
2024-07-26 23:14:12 +0300

    push only dir Apps

--- IShortUrlApp.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShortUrlSrv.App;
-
-public interface IShortUrlApp
-{
-    public string SaveUrl(string longUrl);
-    public string GetLongUrl(string shortUrl);
-    public bool DeleteShortUrl(string shortUrl);
-}
