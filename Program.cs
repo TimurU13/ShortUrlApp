2de7585bb52b4c9008fa0ff8d7d16b5d44b1a3ec
unknown <timurkausmanov4@gmail.com>
2024-07-30 00:11:07 +0300

    save local data before merging

--- Program.cs
+++ Program.cs
@@ -9,16 +9,13 @@ namespace ShortUrlSrv.App
         {
             IUrlDataStore urlDataStore = new UrlDataStore();
             IShortUrlApp shortUrlApp = new ShortUrlApp(urlDataStore);
-
             while (true)
             {
                 Console.WriteLine("Выберите действие:");
                 Console.WriteLine("1 - Получить короткую ссылку");
                 Console.WriteLine("2 - Получить длинную ссылку");
                 Console.WriteLine("3 - Завершить программу");
-
                 string choice = Console.ReadLine();
-
                 switch (choice)
                 {
                     case "1":
@@ -36,18 +33,15 @@ namespace ShortUrlSrv.App
                 }
             }
         }
-
         static void GetShortUrl(IShortUrlApp shortUrlApp)
         {
             Console.WriteLine("Введите оригинальный URL:");
             string longUrl = Console.ReadLine();
-
             if (string.IsNullOrWhiteSpace(longUrl))
             {
                 Console.WriteLine("URL не может быть пустым или содержать только пробелы.");
                 return;
             }
-
             string shortUrl = shortUrlApp.SaveUrl(longUrl);
             if (shortUrl != null)
             {
@@ -58,12 +52,10 @@ namespace ShortUrlSrv.App
                 Console.WriteLine("Не удалось создать короткую ссылку.");
             }
         }
-
         static void GetLongUrl(IShortUrlApp shortUrlApp)
         {
             Console.WriteLine("Введите короткую ссылку:");
             string shortUrl = Console.ReadLine();
-
             try
             {
                 string originalUrl = shortUrlApp.GetLongUrl(shortUrl);
